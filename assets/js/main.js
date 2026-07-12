827177030c5e5cd34547047d0d227f8fd374b582
berkdeveloper <berksarikamis@gmail.com>
2026-07-13 01:49:40 +0300

    all files

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+const languageSelect = document.getElementById("language-select");
+
+if (languageSelect) {
+
+    languageSelect.addEventListener("change", function () {
+
+        const language = this.value;
+
+        const path = window.location.pathname;
+
+        const newPath = path.replace(/^\/(tr|en|ar)/, `/${language}`);
+
+        window.location.href = newPath;
+
+    });
+
+}
+
+
+const header = document.querySelector(".site-header");
+
+let isCompact = false;
+
+function handleHeaderScroll() {
+
+    if (window.innerWidth > 768) {
+
+        header.classList.remove("site-header--compact");
+        isCompact = false;
+        return;
+
+    }
+
+    if (!isCompact && window.scrollY > 100) {
+
+        header.classList.add("site-header--compact");
+        isCompact = true;
+
+    }
+
+    if (isCompact && window.scrollY < 50) {
+
+        header.classList.remove("site-header--compact");
+        isCompact = false;
+
+    }
+
+}
+
+window.addEventListener("scroll", handleHeaderScroll);
+window.addEventListener("resize", handleHeaderScroll);
+
+handleHeaderScroll();
